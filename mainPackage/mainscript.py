d1f3745aa30d1f8bbcc8ca117d068543edc8a5bd
Caleb Iuliano <calebiuliano@gmail.com>
2021-10-23 22:54:32 -0400

    learned to make python packages

---
+++ mainPackage/mainscript.py
@@ -0,0 +1,2 @@
+def report_main():
+  print("A script in main package")
---
+++ myProgram.py
@@ -0,0 +1,6 @@
+
+from mainPackage import mainscript
+from mainPackage.subPackage import subscript
+
+mainscript.report_main()
+subscript.sub_report()
